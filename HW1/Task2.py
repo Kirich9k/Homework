cdf7178867a260f0167dcbd0bd9fd492073ea259
Kirich9k <kirya65@gmail.com>
2023-02-02 16:07:06 +0300

    Task 2 added and accomplished

---
+++ HW1/Task2.py
@@ -0,0 +1,4 @@
+cranes = int(input('Введите количество журавликов: '))
+petrOrSergCranes = cranes / 6
+kateCranes = petrOrSergCranes * 4
+print(f"{petrOrSergCranes} {kateCranes} {petrOrSergCranes}") 
